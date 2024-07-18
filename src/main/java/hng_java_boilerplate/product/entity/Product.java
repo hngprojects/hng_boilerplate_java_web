9d78acfcabf447a929d5891dbd93ed276bcf8734
Lurldgbodex <professionaleditor6@gmail.com>
2024-07-18 15:40:28 +0100

    setup database for project

---
+++ src/main/java/hng_java_boilerplate/product/entity/Product.java
@@ -0,0 +1,57 @@
+package hng_java_boilerplate.product.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import hng_java_boilerplate.user.entity.User;
+import hng_java_boilerplate.util.UUIDGenarator;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "products")
+public class Product {
+    @Id
+    private String id;
+
+    private String name;
+    private String description;
+
+    @ManyToOne
+    @JsonIgnore
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ src/main/java/hng_java_boilerplate/user/entity/User.java
@@ -0,0 +1,87 @@
+package hng_java_boilerplate.user.entity;
+
+import hng_java_boilerplate.organisation.entity.Organisation;
+import hng_java_boilerplate.product.entity.Product;
+import hng_java_boilerplate.profile.entity.Profile;
+import hng_java_boilerplate.util.UUIDGenarator;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    private String id;
+
+    private String name;
+    private String email;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "profile_id", referencedColumnName = "id")
+    private Profile profile;
+
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_organisation",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "organisation_id")
+    )
+    private List<Organisation> organisations;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Product> products;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Profile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(Profile profile) {
+        this.profile = profile;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public List<Organisation> getOrganisations() {
+        return organisations;
+    }
+
+    public void setOrganisations(List<Organisation> organisations) {
+        this.organisations = organisations;
+    }
+}
