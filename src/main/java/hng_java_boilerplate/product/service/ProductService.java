cfd742c2d75b4bf03d1e8a97ecabfa2d850ca098
Amodu <amodudavid777@gmail.com>
2024-07-19 21:24:28 +0100

    Feat: Added an interface for the product service

--- src/main/java/hng_java_boilerplate/product/service/ProductService.java
+++ src/main/java/hng_java_boilerplate/product/service/ProductService.java
@@ -5,7 +5,7 @@ import hng_java_boilerplate.product.entity.Product;
 import java.util.List;
 
 public interface ProductService {
-    
+
     //Method to Search for products with certain criteria, returns a list of products
     List<Product> productsSearch(String name, String category, Double minPrice, Double maxPrice);
 }
