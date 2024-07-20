49cb14dd8daf3e8a2fb2d6a0c5b2dff68c3b327c
Amodu <amodudavid777@gmail.com>
2024-07-20 21:32:50 +0100

    Refactor:  removed extra space

--- src/main/java/hng_java_boilerplate/product/dto/ProductSearchDTO.java
+++ src/main/java/hng_java_boilerplate/product/dto/ProductSearchDTO.java
@@ -11,7 +11,6 @@ import java.util.List;
 @AllArgsConstructor
 @Data
 public class ProductSearchDTO {
-
     private boolean success;
     private int status_code;
     private List<ProductDTO> products;
