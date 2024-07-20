90206b4863c33a125cfa21845980956e2ed33076
Amodu <amodudavid777@gmail.com>
2024-07-20 23:47:59 +0100

    Feat: Added pagination to the repository

--- src/main/java/hng_java_boilerplate/product/repository/ProductRepository.java
+++ src/main/java/hng_java_boilerplate/product/repository/ProductRepository.java
@@ -1,10 +1,11 @@
 package hng_java_boilerplate.product.repository;
 
 import hng_java_boilerplate.product.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, String> {
@@ -14,9 +15,10 @@ public interface ProductRepository extends JpaRepository<Product, String> {
             "(:category IS NULL OR p.category ILIKE %:category%) AND " +
             "(:minPrice IS NULL OR p.price >= :minPrice) AND " +
             "(:maxPrice IS NULL OR p.price <= :maxPrice)")
-    List<Product> searchProducts(@Param("name") String name,
+    Page<Product> searchProducts(@Param("name") String name,
                                  @Param("category") String category,
                                  @Param("minPrice") Double minPrice,
-                                 @Param("maxPrice") Double maxPrice);
+                                 @Param("maxPrice") Double maxPrice,
+                                 Pageable pageable);
 
 }
