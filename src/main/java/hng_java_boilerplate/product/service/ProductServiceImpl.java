4817da5ba7078c1d7cb797aafb50b61585f163c2
Amodu <amodudavid777@gmail.com>
2024-07-20 23:46:25 +0100

    Feat: Added pagination to the service interface and implementation

--- src/main/java/hng_java_boilerplate/product/service/ProductServiceImpl.java
+++ src/main/java/hng_java_boilerplate/product/service/ProductServiceImpl.java
@@ -4,9 +4,9 @@ import hng_java_boilerplate.product.dto.ErrorDTO;
 import hng_java_boilerplate.product.entity.Product;
 import hng_java_boilerplate.product.exceptions.ValidationError;
 import hng_java_boilerplate.product.repository.ProductRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -20,13 +20,13 @@ public class ProductServiceImpl implements ProductService{
     }
 
     @Override
-    public List<Product> productsSearch(String name, String category, Double minPrice, Double maxPrice) {
+    public Page<Product> productsSearch(String name, String category, Double minPrice, Double maxPrice, Pageable pageable) {
         if (name == null || name.isEmpty()) {
             ErrorDTO errorDTO = new ErrorDTO();
             errorDTO.setMessage("name is a required parameter");
             errorDTO.setParameter("name");
             throw new ValidationError(errorDTO);
         }
-            return productRepository.searchProducts(name, category, minPrice, maxPrice);
+            return productRepository.searchProducts(name, category, minPrice, maxPrice, pageable);
     }
 }
