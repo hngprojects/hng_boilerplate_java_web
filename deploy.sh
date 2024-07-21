702740f3e0e16446ee143212c55a6d0f456baa70
Favour Daniel <favourd205@gmail.com>
2024-07-21 03:12:35 +0100

    fix:CI

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 cd ~
-./stop-app.sh
+# ./stop-app.sh
+# systemctl stop java_app.service
 cp application.properties ~/hng_boilerplate_java_web/src/main/resources/
 cp flyway.conf ~/hng_boilerplate_java_web/
 cd ~/hng_boilerplate_java_web/
 mvn dependency:resolve
 ./mvnw clean install
-nohup ./mvnw spring-boot:run > app.log 2>&1 &
+# # nohup ./mvnw spring-boot:run > app.log 2>&1 &
+# sysmtemctl start java_app.service
