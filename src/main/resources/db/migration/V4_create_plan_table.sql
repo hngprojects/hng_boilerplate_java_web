5fc1b0506e58f3e8d533fb1d617060fdc68d0c03
adewalekujore <adewalekujore6@gmail.com>
2024-07-22 10:39:40 +0100

    sql type fix

--- src/main/resources/db/migration/V4_create_plan_table.sql
+++ src/main/resources/db/migration/V4_create_plan_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE plans
     id            VARCHAR(255) PRIMARY KEY,
     name          VARCHAR(255)  NOT NULL UNIQUE,
     description   VARCHAR(255)  NOT NULL,
-    price         DECIMAL(0, 2) NOT NULL,
+    price         DECIMAL(10, 2) NOT NULL,
     duration      INTEGER       NOT NULL,
     duration_unit VARCHAR(255)  NOT NULL
 );
