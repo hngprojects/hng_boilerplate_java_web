e4e32968935e07d0f5328a33eaefee8b73818c9c
ChielokaCode <realmatec01@gmail.com>
2024-07-24 07:32:51 +0100

    Feat: Added Sale and SaleItem entity

---
+++ src/main/java/hng_java_boilerplate/sale/entity/SaleItem.java
@@ -0,0 +1,28 @@
+package hng_java_boilerplate.sale.entity;
+
+import hng_java_boilerplate.product.entity.Product;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SaleItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "sale_id", nullable = false)
+    private Sale sale;
+
+    @Column(nullable = false)
+    private Integer quantity;
+}
