40c424abd8e8165e8c6d26f579c79f7b94942ca8
Lurldgbodex <professionaleditor6@gmail.com>
2024-07-24 17:59:53 +0100

    fix: load full context in test to fix test break

--- src/test/java/hng_java_boilerplate/organisation/controller/OrganisationControllerTest.java
+++ src/test/java/hng_java_boilerplate/organisation/controller/OrganisationControllerTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -21,8 +21,8 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@AutoConfigureMockMvc(addFilters = false)
-@WebMvcTest(OrganisationController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class OrganisationControllerTest {
     @Autowired
     private MockMvc mockMvc;
