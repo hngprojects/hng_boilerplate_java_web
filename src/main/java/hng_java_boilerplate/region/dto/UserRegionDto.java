b7e0e97963494af3b2cac577b18a07d66b0b6492
unyimeudoh <unyimeudemy20@gmail.com>
2024-07-24 13:54:31 +0100

    swagger documentation added

--- src/main/java/hng_java_boilerplate/region/dto/UserRegionDto.java
+++ src/main/java/hng_java_boilerplate/region/dto/UserRegionDto.java
@@ -2,6 +2,7 @@ package hng_java_boilerplate.region.dto;
 
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,17 +15,22 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Schema(description = "Data transfer object for user region")
 public class UserRegionDto {
 
+    @Schema(description = "ID of the region", example = "1")
     @JsonProperty("region_id")
     private Integer regionId;
 
+    @Schema(description = "ID of the user", example = "123e4567-e89b-12d3-a456-426614174000")
     @JsonProperty("user_id")
     private UUID userId;
 
+    @Schema(description = "Name of the region", example = "North Region")
     @JsonProperty("region_name")
     private String regionName;
 
+    @Schema(description = "Country code of the region", example = "US")
     @JsonProperty("country_code")
     private String countryCode;
 }
--- src/main/java/hng_java_boilerplate/region/entity/UserRegionEntity.java
+++ src/main/java/hng_java_boilerplate/region/entity/UserRegionEntity.java
@@ -1,6 +1,7 @@
 package hng_java_boilerplate.region.entity;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,21 +14,25 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "user_region")
+@Schema(description = "Entity representing user region information in the database")
 public class UserRegionEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "region_id", nullable = false)
+    @Schema(description = "Unique identifier for the user region record", example = "1")
     private Integer regionId;
 
     @Column(name = "user_id", nullable = false)
+    @Schema(description = "Unique identifier of the user", example = "123e4567-e89b-12d3-a456-426614174000")
     private UUID userId;
 
     @Column(name = "region_name", nullable = false)
+    @Schema(description = "Name of the region", example = "North Region")
     private String regionName;
 
     @Column(name = "region_code", nullable = false)
+    @Schema(description = "Code of the region", example = "NR")
     private String regionCode;
 }
 
