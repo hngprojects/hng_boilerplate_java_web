f2b72da58b4669e4d0c5a71e91d050053ba347be
unyimeudoh <unyimeudemy20@gmail.com>
2024-07-24 13:54:31 +0100

    swagger documentation added

--- src/main/java/hng_java_boilerplate/region/dto/RegionDto.java
+++ src/main/java/hng_java_boilerplate/region/dto/RegionDto.java
@@ -2,6 +2,7 @@ package hng_java_boilerplate.region.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,30 +14,40 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Schema(description = "Data transfer object for Region")
 public class RegionDto {
 
+    @Schema(description = "Code of the region", example = "123")
     @JsonProperty("region_code")
     private String regionCode;
 
+    @Schema(description = "Name of the region", example = "North Region")
     @JsonProperty("region_name")
     private String regionName;
 
+    @Schema(description = "Status of the region", example = "1")
     @JsonProperty("status")
     private int status;
 
+    @Schema(description = "Creation timestamp of the region", example = "2023-01-01T12:00:00")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     @JsonProperty("created_on")
     private LocalDateTime createdOn;
 
+    @Schema(description = "User who created the region", example = "admin")
     @JsonProperty("created_by")
     private String createdBy;
 
+    @Schema(description = "Last modification timestamp of the region", example = "2023-01-01T12:00:00")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     @JsonProperty("modified_on")
     private LocalDateTime modifiedOn;
 
+    @Schema(description = "User who last modified the region", example = "admin")
     @JsonProperty("modified_by")
     private String modifiedBy;
+
+
 }
 
 
--- src/main/java/hng_java_boilerplate/region/dto/RegionErrorResponseDto.java
+++ src/main/java/hng_java_boilerplate/region/dto/RegionErrorResponseDto.java
@@ -1,6 +1,7 @@
 package hng_java_boilerplate.region.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
@@ -10,11 +11,14 @@ import lombok.Builder;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Schema(description = "Data transfer object for Region error response")
 public class RegionErrorResponseDto {
 
+    @Schema(description = "HTTP status code of the error", example = "404")
     @JsonProperty("status_code")
     private String statusCode;
 
+    @Schema(description = "Error message", example = "Region not found")
     @JsonProperty("message")
     private String message;
 }
