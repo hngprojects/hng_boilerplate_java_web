861abbad690c00c431c31a9f355efcbf8a38d6bd
callmeInnocent <iamwalter300@mail.com>
2024-07-25 00:18:01 +0100

    feat: add Users to Organizations API feature

--- src/main/java/hng_java_boilerplate/organisation/Controller/OrganisationController.java
+++ src/main/java/hng_java_boilerplate/organisation/Controller/OrganisationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/api/organisations")
+@RequestMapping("/api/v1/organisations")
 public class OrganisationController {
     @Autowired
     private OrganisationService organisationService;
