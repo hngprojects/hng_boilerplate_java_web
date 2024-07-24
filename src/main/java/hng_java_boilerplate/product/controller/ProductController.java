c1fa05b38987f421a09942b118a0dbadf71c7752
trini7y <okekedesmond71@gmail.com>
2024-07-24 21:20:28 +0100

    Add product implementation

--- src/main/java/hng_java_boilerplate/product/controller/ProductController.java
+++ src/main/java/hng_java_boilerplate/product/controller/ProductController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/v1/products")
