5c7e2560004681d453fb85fc83770d32a124e099
ChielokaCode <realmatec01@gmail.com>
2024-07-24 07:29:21 +0100

    Feat: Added Response DTO

--- src/main/java/hng_java_boilerplate/sale/dto/ResponseDTO.java
+++ src/main/java/hng_java_boilerplate/sale/dto/ResponseDTO.java
@@ -1,5 +1,24 @@
 package hng_java_boilerplate.sale.dto;
 
-public class ResponseDTO {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseDTO {
+    private String status;
+    private int status_code;
+    private List<String> month;
+    private List<Double> totalSales;
+    private List<Integer> totalSalesInDegree;
+    private Long total_users;
+    private Long active_users;
+    private Long new_users;
+    private Long total_revenue;
 }
