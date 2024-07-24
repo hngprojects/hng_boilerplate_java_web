d2ba97c73128cccefc91349fc0ac01c7d5ef6ac0
ChielokaCode <realmatec01@gmail.com>
2024-07-24 07:27:42 +0100

    Feat: Added ErrorResponse DTO

--- src/main/java/hng_java_boilerplate/sale/dto/ErrorResponse.java
+++ src/main/java/hng_java_boilerplate/sale/dto/ErrorResponse.java
@@ -1,5 +1,24 @@
 package hng_java_boilerplate.sale.dto;
 
-public class ErrorResponse {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private String status;
+    private int status_code;
+    private String error;
+    private String message;
+    private ErrorDetails details;
+
+    public ErrorResponse(String status, int status_code, String error, String message){
+        this.status = status;
+        this.status_code = status_code;
+        this.error = error;
+        this.message = message;
+    }
 }
