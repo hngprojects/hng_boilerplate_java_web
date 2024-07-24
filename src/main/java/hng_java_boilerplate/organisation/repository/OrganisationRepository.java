f04bfff0d94621aac840e65d0244d706bcaade94
Okpala Emmanuel <140505369+merison200@users.noreply.github.com>
2024-07-24 19:28:01 +0100

    srcfirstcommit

--- src/main/java/hng_java_boilerplate/organisation/repository/OrganisationRepository.java
+++
@@ -1,7 +0,0 @@
-package hng_java_boilerplate.organisation.repository;
-
-import hng_java_boilerplate.organisation.entity.Organisation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrganisationRepository extends JpaRepository<Organisation, String> {
-}
