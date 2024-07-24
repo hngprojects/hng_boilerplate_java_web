f724e0441a940329421dbbd79a09233fe0348935
ChielokaCode <realmatec01@gmail.com>
2024-07-24 07:54:12 +0100

    Feat: Added Sale Service - getPieChart

--- src/main/java/hng_java_boilerplate/sale/service/SaleService.java
+++ src/main/java/hng_java_boilerplate/sale/service/SaleService.java
@@ -2,4 +2,5 @@ package hng_java_boilerplate.sale.service;
 
 public interface SaleService {
     Object getSummary();
+    Object getPieChartData();
 }
--- src/main/java/hng_java_boilerplate/sale/service/serviceImpl/SalesServiceImpl.java
+++ src/main/java/hng_java_boilerplate/sale/service/serviceImpl/SalesServiceImpl.java
@@ -2,6 +2,7 @@ package hng_java_boilerplate.sale.service.serviceImpl;
 
 import hng_java_boilerplate.sale.dto.ErrorDetails;
 import hng_java_boilerplate.sale.dto.ErrorResponse;
+import hng_java_boilerplate.sale.dto.MonthlySalesDTO;
 import hng_java_boilerplate.sale.dto.ResponseDTO;
 import hng_java_boilerplate.sale.entity.Sale;
 import hng_java_boilerplate.sale.repository.SaleRepository;
@@ -20,6 +21,7 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class SalesServiceImpl implements SaleService {
     @Autowired
@@ -100,6 +102,65 @@ public class SalesServiceImpl implements SaleService {
                     404,
                     "ResourceNotFound",
                     "There was an error fetching the summary data",
+                    errorDetails
+            );
+        }
+    }
+
+    @Override
+    public Object getPieChartData() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication.isAuthenticated()) || !(authentication instanceof UsernamePasswordAuthenticationToken)) {
+            return new UserNotFoundException("User not authenticated or token is invalid.");
+        }
+        String username = authentication.getName();
+        Optional<User> user = userRepository.findByEmail(username);
+        if (user.isEmpty()) {
+            return new UserNotFoundException("User not authenticated or token is invalid.");
+        }
+        try {
+            List<Sale> sales = salesRepository.findAll();
+            if (sales.isEmpty()) {
+                ErrorDetails errorDetails = new ErrorDetails();
+                errorDetails.setChartType("pie");
+                return new ErrorResponse(
+                        "false",
+                        404,
+                        "DataFetchError",
+                        "There was an error fetching the pie chart data",
+                        errorDetails
+                );
+            }
+            List<MonthlySalesDTO> monthlySalesDTOs = sales.stream()
+                    .map(s -> new MonthlySalesDTO(s.getMonth(), s.getTotalSale()))
+                    .collect(Collectors.toList());
+            double totalSales = monthlySalesDTOs.stream()
+                    .mapToDouble(MonthlySalesDTO::getTotalSales)
+                    .sum();
+            List<Double> totalSalesList = monthlySalesDTOs.stream()
+                    .map(MonthlySalesDTO::getTotalSales)
+                    .collect(Collectors.toList());
+            List<Integer> degreesList = monthlySalesDTOs.stream()
+                    .map(dto -> (int) ((dto.getTotalSales() / totalSales) * 360))
+                    .collect(Collectors.toList());
+            List<String> monthList = monthlySalesDTOs.stream()
+                    .map(MonthlySalesDTO::getMonth)
+                    .collect(Collectors.toList());
+            ResponseDTO responseDTO = new ResponseDTO();
+            responseDTO.setStatus("true");
+            responseDTO.setStatus_code(200);
+            responseDTO.setMonth(monthList);
+            responseDTO.setTotalSales(totalSalesList);
+            responseDTO.setTotalSalesInDegree(degreesList);
+            return responseDTO;
+        } catch (Exception e) {
+            ErrorDetails errorDetails = new ErrorDetails();
+            errorDetails.setChartType("pie");
+            return new ErrorResponse(
+                    "false",
+                    404,
+                    "DataFetchError",
+                    "There was an error fetching the pie chart data",
                     errorDetails
             );
         }
