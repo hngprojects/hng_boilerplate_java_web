07b146958281f06d6ed464a272da0b8ae89d9b1c
ChielokaCode <realmatec01@gmail.com>
2024-07-25 06:38:44 +0100

    test: Add unit test for sale service

--- src/test/java/hng_java_boilerplate/sale/service/saleServiceImpl/SaleServiceImplTest.java
+++ src/test/java/hng_java_boilerplate/sale/service/saleServiceImpl/SaleServiceImplTest.java
@@ -24,7 +24,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
-public class SalesServiceImplTest {
+public class SaleServiceImplTest {
 
     @InjectMocks
     private SaleServiceImpl salesService;
