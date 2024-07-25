c6f3aefb7d864c402bcd9e6bb925f0306161446b
the_zen <alayandezainab64@gmail.com>
2024-07-25 12:58:19 +0100

    feat: all flyway scripts added in one sql file

--- src/main/resources/db/migration/V6__add_two_fa_columns_and_backup_codes_table.sql
+++
@@ -1,8 +0,0 @@
--- V6__add_two_fa_columns_and_backup_codes_table.sql
--- Create the two_fa_backup_codes table
-CREATE TABLE two_fa_backup_codes (
-                                     user_id VARCHAR(255) NOT NULL,
-                                     backup_code VARCHAR(255) NOT NULL,
-                                     PRIMARY KEY (user_id, backup_code),
-                                     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
