84bebc9ec3c967a344f486afdc813eddad85443c
ChielokaCode <realmatec01@gmail.com>
2024-07-25 06:42:01 +0100

    feat: Add sale database migration script

---
+++ src/main/resources/db/migration/V5__sale_database.sql
@@ -0,0 +1,18 @@
+-- V1__Create_sale_table.sql
+CREATE TABLE sale (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    total_sale DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+-- V1__Create_saleItem_table.sql
+CREATE TABLE sale_item (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    product_id VARCHAR(255) NOT NULL,
+    sale_id BIGINT NOT NULL,
+    quantity INTEGER NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES products(id),
+    FOREIGN KEY (sale_id) REFERENCES sale(id)
+);
