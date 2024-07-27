a6410b3758f6526ed634364e66f45a93bb92df1b
Marvellous Giegbefumwen <igmarvvvi@gmail.com>
2024-07-27 17:00:05 +0100

    create v8__create_waitlist_table

--- src/main/resources/db/migration/V1__init_database.sql
+++ src/main/resources/db/migration/V1__init_database.sql
@@ -44,9 +44,4 @@ CREATE TABLE user_organisation (
     FOREIGN KEY (organisation_id) REFERENCES organisations(id)
 );
 
--- V1__Create_waitlist_table.sql
-CREATE TABLE waitlist (
-    id UUID PRIMARY KEY,
-    full_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL
-);
+
