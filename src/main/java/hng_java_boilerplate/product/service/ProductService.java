59404bf43c5d0ad5fbbe993a3667992c6c9f3235
Prebor97 <prebstamar@gmail.com>
2024-07-27 15:07:24 +0100

    feat:delete product feature

--- src/main/java/hng_java_boilerplate/product/service/ProductService.java
+++ src/main/java/hng_java_boilerplate/product/service/ProductService.java
@@ -1,6 +1,8 @@
 package hng_java_boilerplate.product.service;
 
 import hng_java_boilerplate.product.entity.Product;
+import hng_java_boilerplate.product.exceptions.ProductNotFoundException;
+import hng_java_boilerplate.product.exceptions.UnauthorizedAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -10,4 +12,5 @@ public interface ProductService {
 
     //Method to Search for products with certain criteria, returns a list of products
     Page<Product> productsSearch(String name, String category, Double minPrice, Double maxPrice, Pageable pageable);
+    void deleteProductById(String id) throws ProductNotFoundException, UnauthorizedAccessException;
 }
