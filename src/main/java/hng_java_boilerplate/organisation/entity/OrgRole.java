cd159b7bfdd4a0d19ea1d257097e8e9336366f60
victoradepoju <victoradepoju30@gmail.com>
2024-07-29 02:42:48 +0100

    feat: create role and permission table and migration file for organisation

---
+++ src/main/java/hng_java_boilerplate/organisation/entity/OrgRole.java
@@ -0,0 +1,40 @@
+package hng_java_boilerplate.organisation.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "org_roles")
+public class OrgRole {
+    @Id
+    private String id;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "org_role_permissions",
+            joinColumns = @JoinColumn(name = "org_role_id"),
+            inverseJoinColumns = @JoinColumn(name = "org_permission_id")
+    )
+    private Set<OrgPermission> permissions = new HashSet<>();
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
