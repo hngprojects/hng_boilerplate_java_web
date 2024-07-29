e383575a4e030880c145de0e83f16348fd59f95c
Amodu David <104693505+Am0du@users.noreply.github.com>
2024-07-29 16:47:54 +0100

    Revert "Feat: Implement all endpoints for Testimonials"

--- src/main/java/hng_java_boilerplate/testimonials/repository/TestimonialRepository.java
+++
@@ -1,7 +0,0 @@
-package hng_java_boilerplate.testimonials.repository;
-
-import hng_java_boilerplate.testimonials.entity.Testimonial;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestimonialRepository extends JpaRepository<Testimonial, String> {
-}
