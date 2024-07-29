b731d7f509aad1ee193f4ebdb05f5cd9ae79cdb9
Fifetoyi <fifetoyi@gmail.com>
2024-07-29 10:22:40 +0100

    feat: create the testimonial service

---
+++ src/main/java/hng_java_boilerplate/testimonials/service/TestimonialService.java
@@ -0,0 +1,25 @@
+package hng_java_boilerplate.testimonials.service;
+
+import hng_java_boilerplate.testimonials.entity.Testimonial;
+import hng_java_boilerplate.testimonials.repository.TestimonialRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
+public class TestimonialService {
+
+    private final TestimonialRepository testimonialRepository;
+
+    public Testimonial createTestimonial(String userId, String name, String content) {
+        Testimonial testimonial = new Testimonial();
+        testimonial.setUserId(userId);
+        testimonial.setName(name);
+        testimonial.setContent(content);
+        testimonial.setCreatedAt(LocalDate.now());
+
+        return testimonialRepository.save(testimonial);
+    }
+}
