27442b9569f029c435788796ff47a9913d22e38e
Fifetoyi <fifetoyi@gmail.com>
2024-07-29 11:59:21 +0100

    fix: retrieve logged in user properly

--- src/main/java/hng_java_boilerplate/testimonials/controller/TestimonialController.java
+++ src/main/java/hng_java_boilerplate/testimonials/controller/TestimonialController.java
@@ -5,15 +5,17 @@ import hng_java_boilerplate.testimonials.dto.TestimonialRequestDto;
 import hng_java_boilerplate.testimonials.dto.TestimonialResponseDto;
 import hng_java_boilerplate.testimonials.entity.Testimonial;
 import hng_java_boilerplate.testimonials.service.TestimonialService;
+import hng_java_boilerplate.user.entity.User;
+import hng_java_boilerplate.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/v1/testimonials")
@@ -21,11 +23,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestimonialController {
 
     private final TestimonialService testimonialService;
+    private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<?> createTestimonial(@AuthenticationPrincipal UserDetails userDetails,
-                                               @Valid @RequestBody TestimonialRequestDto request) {
-        Testimonial testimonial = testimonialService.createTestimonial(userDetails.getUsername(), request.getName(), request.getContent());
+    public ResponseEntity<?> createTestimonial(@Valid @RequestBody TestimonialRequestDto request) {
+        User loggedInUser = userService.getLoggedInUser();
+
+        if (loggedInUser == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not authenticated");
+        }
+        Testimonial testimonial = testimonialService.createTestimonial(loggedInUser.getId(), request.getName(), request.getContent());
 
         TestimonialDataDto testimonialData = new TestimonialDataDto(testimonial.getUserId(), testimonial.getName(), testimonial.getContent(), testimonial.getCreatedAt());
 
