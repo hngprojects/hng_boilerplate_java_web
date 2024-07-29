66f27165d44950af38d1330d8e3845aaa8baf95c
Prebor97 <prebstamar@gmail.com>
2024-07-29 20:02:49 +0100

    feat:Implemented retrieve and update about page feature

---
+++ src/main/java/hng_java_boilerplate/externalPages/repository/AboutPageRepository.java
@@ -0,0 +1,6 @@
+package hng_java_boilerplate.externalPages;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AboutPageRepository extends JpaRepository<AboutPage, Long> {
+}
