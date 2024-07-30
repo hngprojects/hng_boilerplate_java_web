4a2be546308a145025b28f55db8795f71ce21c7e
Amodu <amodudavid777@gmail.com>
2024-07-30 21:32:18 +0100

    Feat: added a /metrics endpoint for prometheus

---
+++ src/main/java/hng_java_boilerplate/config/MetricsConfig.java
@@ -0,0 +1,15 @@
+package hng_java_boilerplate.config;
+
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MetricsConfig {
+
+    @Bean
+    public PrometheusMeterRegistry prometheusMeterRegistry() {
+        return new PrometheusMeterRegistry(io.micrometer.prometheus.PrometheusConfig.DEFAULT);
+    }
+
+}
