ef271b1620ddf903602aa5810cfa18bbcaddc900
Amodu David <104693505+Am0du@users.noreply.github.com>
2024-07-30 08:49:56 +0100

    Update PlanController.java

--- src/main/java/hng_java_boilerplate/plans/controller/PlanController.java
+++ src/main/java/hng_java_boilerplate/plans/controller/PlanController.java
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
-@RequestMapping("api/v1/plans")
-@RequiredArgsConstructor
 public class PlanController {
 
     private final PlanService planService;
