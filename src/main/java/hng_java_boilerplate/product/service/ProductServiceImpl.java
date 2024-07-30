9f2ce25d369ff2b7bdffaf3cf2aa03a2985e4154
Chinecherem <chinecheremubawike@gmail.com>
2024-07-30 09:31:27 +0100

    Feat: Create Product Api Endpoint

--- src/main/java/hng_java_boilerplate/product/service/ProductServiceImpl.java
+++ src/main/java/hng_java_boilerplate/product/service/ProductServiceImpl.java
@@ -11,13 +11,9 @@ import hng_java_boilerplate.user.repository.UserRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.UUID;
 
 @Service
-
 public class ProductServiceImpl implements ProductService{
     private ProductRepository productRepository;
     private UserRepository userRepository;
