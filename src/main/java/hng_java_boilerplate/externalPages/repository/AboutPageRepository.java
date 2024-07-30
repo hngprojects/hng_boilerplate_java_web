e9071bec310a626e49eb3ef78c9b335049beffb1
Gbodi Segun Clement <42384410+lurldgbodex@users.noreply.github.com>
2024-07-30 09:25:07 +0100

    Revert "Feat/retrieve update about page"

--- src/main/java/hng_java_boilerplate/externalPages/repository/AboutPageRepository.java
+++
@@ -1,7 +0,0 @@
-package hng_java_boilerplate.externalPages.repository;
-
-import hng_java_boilerplate.externalPages.entity.AboutPage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AboutPageRepository extends JpaRepository<AboutPage, Long> {
-}
--- src/main/java/hng_java_boilerplate/externalPages/repository/ServiceRepository.java
+++
@@ -1,7 +0,0 @@
-package hng_java_boilerplate.externalPages.repository;
-
-import hng_java_boilerplate.externalPages.entity.ServiceEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceRepository extends JpaRepository<ServiceEntity, Long> {
-}
