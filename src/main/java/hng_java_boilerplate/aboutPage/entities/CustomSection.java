cece88c4570b82f30c7c69f7778253db53095977
Prebor97 <prebstamar@gmail.com>
2024-07-31 08:20:28 +0100

    fixed table issue

--- src/main/java/hng_java_boilerplate/aboutPage/entities/CustomSection.java
+++ src/main/java/hng_java_boilerplate/aboutPage/entities/CustomSection.java
@@ -23,6 +23,6 @@ public class CustomSection {
     @OneToOne
     @JoinColumn(name = "about_page_id")
     @JsonBackReference
-    private AboutPage aboutPage;
+    private AboutPage about_page;
 
 }
