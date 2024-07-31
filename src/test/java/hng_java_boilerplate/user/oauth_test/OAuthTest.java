cca3ecb2cd3bda5947d2877b3609c8b5a5a974e1
ofektom <okpohot@gmail.com>
2024-07-31 22:18:25 +0100

    feat: create-google-oauth-update

--- src/test/java/hng_java_boilerplate/user/oauth_test/OAuthTest.java
+++ src/test/java/hng_java_boilerplate/user/oauth_test/OAuthTest.java
@@ -18,9 +18,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.time.LocalDateTime;
+
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 class OAuthTest {
@@ -48,21 +49,29 @@ class OAuthTest {
                 "John",
                 "Doe",
                 "john.doe@example.com",
+                "USER_ROLE",
+                "mockedImageUrl",
                 now
         );
 
         ResponseData responseData = new ResponseData("mockedJwtToken", userResponse);
-
         ApiResponse apiResponse = new ApiResponse(HttpStatus.OK.value(), "Login Successful!", responseData);
         ResponseEntity<ApiResponse> responseEntity = new ResponseEntity<>(apiResponse, HttpStatus.OK);
 
         when(googleJwtUtils.googleOauthUserJWT(anyString())).thenReturn(responseEntity);
 
-        mockMvc.perform(get("/api/v1/auth/google/{tkn}", simulatedToken)
+        mockMvc.perform(post("/api/v1/auth/google")
+                        .param("token", simulatedToken)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.data.token").value("mockedJwtToken"))
                 .andExpect(jsonPath("$.status").value(HttpStatus.OK.value()))
-                .andExpect(jsonPath("$.message").value("Login Successful!"));
+                .andExpect(jsonPath("$.message").value("Login Successful!"))
+                .andExpect(jsonPath("$.data.token").value("mockedJwtToken"))
+                .andExpect(jsonPath("$.data.user.id").value("1"))
+                .andExpect(jsonPath("$.data.user.first_name").value("John"))
+                .andExpect(jsonPath("$.data.user.last_name").value("Doe"))
+                .andExpect(jsonPath("$.data.user.email").value("john.doe@example.com"))
+                .andExpect(jsonPath("$.data.user.role").value("USER_ROLE"))
+                .andExpect(jsonPath("$.data.user.imr_url").value("mockedImageUrl"));
     }
 }
