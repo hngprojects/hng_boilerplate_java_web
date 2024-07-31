01e64d18df30b589333c6fb4026de4b0722afd26
Prebor97 <prebstamar@gmail.com>
2024-07-31 08:25:10 +0100

    fixed table issue

--- src/main/java/hng_java_boilerplate/aboutPage/services/AboutPageInitialization.java
+++ src/main/java/hng_java_boilerplate/aboutPage/services/AboutPageInitialization.java
@@ -33,7 +33,7 @@ public class AboutPageInitialization {
             aboutPage.setIntroduction("Trained to give you the best boiler codes");
             aboutPageRepository.save(aboutPage);
 
-            customSection.setAboutPage(aboutPage);
+            customSection.setAbout_page(aboutPage);
             customSectionRepository.save(customSection);
 
             statEntity.setYears_in_business(10);
