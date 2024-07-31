591b511f210553e4c3cb5243979a39442fe40a34
Prebor97 <prebstamar@gmail.com>
2024-07-31 09:23:22 +0100

    fixed table issue

--- src/main/resources/db/migration/v16__setup_about_page.sql
+++ src/main/resources/db/migration/v16__setup_about_page.sql
@@ -1,5 +1,5 @@
 -- Create the aboutPage table
-CREATE TABLE IF NOT EXISTS about_page (
+CREATE TABLE IF NOT EXISTS abouts (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     introduction TEXT
