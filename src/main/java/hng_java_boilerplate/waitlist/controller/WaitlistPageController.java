130e01a5e26d6c1ad60cd9e2872fe8b0c3c05af1
Fifetoyi <fifetoyi@gmail.com>
2024-08-01 13:28:11 +0100

    fix: refactor endpoint

--- src/main/java/hng_java_boilerplate/waitlist/controller/WaitlistPageController.java
+++ src/main/java/hng_java_boilerplate/waitlist/controller/WaitlistPageController.java
@@ -17,7 +17,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/waitlist/pages")
+@RequestMapping("/api/v1/waitlist/config")
 @Tag(name = "Waitlist Pages")
 @PreAuthorize("hasRole('ADMIN')")
 public class WaitlistPageController {
