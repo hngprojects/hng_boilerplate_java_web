35bad8cb2779e51a7a4c5d0723058f3682425a1c
Nwachukwu Chinaza Esther <105193755+NecyNazy@users.noreply.github.com>
2024-08-01 16:48:01 +0100

    removed notificationsettings

--- src/main/resources/db/migration/V5__notification_settings_database.sql
+++
@@ -1,8 +0,0 @@
--- V5__Create_notification_settings_table.sql
-CREATE TABLE notification_settings (
-id VARCHAR(36) PRIMARY KEY,
-user_id VARCHAR(36) NOT NULL,
-email_notification BOOLEAN NOT NULL,
-push_notification BOOLEAN NOT NULL,
-sms_notification BOOLEAN NOT NULL
-);
