3e89e8fb735fc3b8f403ec486c8aeab937746970
jaminel <jamineltechnology@gmail.com>
2024-08-02 16:05:29 +0100

    Ignore application.properties file

--- src/main/resources/db/migration/V25__create_role_permissions_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE role_permissions (
-                                  role_id UUID NOT NULL,
-                                  permission_id UUID NOT NULL,
-                                  PRIMARY KEY (role_id, permission_id),
-                                  CONSTRAINT fk_role FOREIGN KEY(role_id) REFERENCES roles(id),
-                                  CONSTRAINT fk_permission FOREIGN KEY(permission_id) REFERENCES permissions(id)
-);
