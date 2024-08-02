32132b1166198ee26fea58b66fbd156c68e96fb0
callmeInnocent <iamwalter300@mail.com>
2024-08-02 15:20:41 +0100

    Fixed

--- src/test/java/hng_java_boilerplate/blogCategory/e2e_test/CategoryBlogControllerTest.java
+++ src/test/java/hng_java_boilerplate/blogCategory/e2e_test/CategoryBlogControllerTest.java
@@ -2,6 +2,7 @@ package hng_java_boilerplate.blogCategory.e2e_test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hng_java_boilerplate.blogCategory.dto.CreateBlogCategoryRequestDTO;
+import hng_java_boilerplate.blogCategory.dto.CreateBlogCategoryResponseDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,8 +10,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -23,17 +29,25 @@ public class CategoryBlogControllerTest {
     private ObjectMapper objectMapper;
 
     @Test
-    @WithMockUser(username = "omoyibo@gmail.com", roles = "ADMIN")
-    public void testCreateBlogCategory() throws Exception {
+    @WithMockUser(roles = "ADMIN")
+    public void testCreateBlogCategorySuccess() throws Exception {
 
         CreateBlogCategoryRequestDTO requestDTO = new CreateBlogCategoryRequestDTO();
-        requestDTO.setName("love Category");
+        requestDTO.setName("Testssv Category");
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/create/blog-categories")
+
+        MvcResult result = mockMvc.perform(
+                post("/api/v1/create/blog-categories")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(requestDTO)))
-                .andExpect(MockMvcResultMatchers.status().isCreated())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.data.name.name").value("love Category"));
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.data.name.name").value("Testssv Category"))
+                .andReturn();
+
+
+        String responseContent = result.getResponse().getContentAsString();
+        CreateBlogCategoryResponseDTO responseDTO = objectMapper.readValue(responseContent, CreateBlogCategoryResponseDTO.class);
+        assertEquals("Testssv Category", responseDTO.getData().getName().getName());
     }
 }
