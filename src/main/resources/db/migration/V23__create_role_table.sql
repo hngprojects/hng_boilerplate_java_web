b41c768664d039ad534d8e8deff8131d28c14868
jaminel <jamineltechnology@gmail.com>
2024-08-02 16:06:31 +0100

    feat: organisation<#126>-<Add all  functionality for  organisation

--- src/main/resources/db/migration/V23__create_role_table.sql
+++ src/main/resources/db/migration/V23__create_role_table.sql
@@ -5,3 +5,10 @@ CREATE TABLE roles (
                        organisation_id UUID NOT NULL,
                        CONSTRAINT fk_organisation FOREIGN KEY(organisation_id) REFERENCES organisations(id)
 );
+CREATE TABLE role_permissions (
+                                  role_id UUID NOT NULL,
+                                  permission_id UUID NOT NULL,
+                                  PRIMARY KEY (role_id, permission_id),
+                                  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles (id),
+                                  CONSTRAINT fk_permission FOREIGN KEY (permission_id) REFERENCES permissions (id)
+);
