5eef78c2f907395039b0227a71145b39693e86ae
callmeInnocent <iamwalter300@mail.com>
2024-08-02 16:53:18 +0100

    fixx

--- src/test/java/hng_java_boilerplate/newsletter/e2e_test/NewsletterControllerTest.java
+++ src/test/java/hng_java_boilerplate/newsletter/e2e_test/NewsletterControllerTest.java
@@ -1,14 +1,11 @@
 package hng_java_boilerplate.newsletter.e2e_test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import hng_java_boilerplate.email.EmailServices.EmailProducerService;
 import hng_java_boilerplate.newsletter.dto.NewsletterEmailRequestDto;
-import hng_java_boilerplate.newsletter.service.NewsletterService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -25,11 +22,7 @@ public class NewsletterControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private NewsletterService newsletterService;
 
-    @MockBean
-    private EmailProducerService emailProducerService;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -45,16 +38,6 @@ public class NewsletterControllerTest {
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 
-    @Test
-    public void testJoinNewsletter_existingEmail() throws Exception {
-        NewsletterEmailRequestDto subscriptionEmailRequestDto = new NewsletterEmailRequestDto("test00@gmail.com");
-
-        mockMvc.perform(post("/api/v1/pages/newsletter")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(subscriptionEmailRequestDto)))
-                .andExpect(status().isBadRequest())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
-    }
 
     @Test
     @WithMockUser(roles = "ADMIN")
