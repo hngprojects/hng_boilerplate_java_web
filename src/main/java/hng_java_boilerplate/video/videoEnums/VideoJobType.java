e9bc5677db4111112d87beee9ddd86c7117ad1da
Amodu <amodudavid777@gmail.com>
2024-08-06 03:48:21 +0100

    Feat: Added Enums

---
+++ src/main/java/hng_java_boilerplate/video/videoEnums/VideoJobType.java
@@ -0,0 +1,20 @@
+package hng_java_boilerplate.video.videoEnums;
+
+public enum VideoJobType {
+
+    MERGE_VIDEO("merge video");
+    private final String status;
+
+    VideoJobType(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return status;
+    }
+}
