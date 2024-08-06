2ac7242abaf8faa26170294ff277d3ad23ddb160
Amodu <amodudavid777@gmail.com>
2024-08-06 13:04:41 +0100

    Fix: changed JobId from UUID to String

--- src/main/java/hng_java_boilerplate/video/repository/VideoRepository.java
+++ src/main/java/hng_java_boilerplate/video/repository/VideoRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface VideoRepository extends JpaRepository<VideoSuite, UUID> {
+public interface VideoRepository extends JpaRepository<VideoSuite,String> {
 
 }
