24248fd7f7d45bd4086341c7952c09209259fd8b
callmeInnocent <iamwalter300@mail.com>
2024-08-08 17:49:56 +0100

    fix: repo synced

--- src/main/java/hng_java_boilerplate/config/RabbitMQConfig.java
+++ src/main/java/hng_java_boilerplate/config/RabbitMQConfig.java
@@ -1,55 +1,36 @@
 package hng_java_boilerplate.config;
 
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-
-import java.util.Properties;
 
 @Configuration
 public class RabbitMQConfig {
 
-
-    public static final String QUEUE_NAME = "emailQueue";
-    @Bean
-    public Jackson2JsonMessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
+    @Value("${rabbitmq.queue.email}")
+    private String emailQueueName;
 
     @Value("${rabbitmq.queue.concat}")
     private String videoConcat;
 
+    @Value("${rabbitmq.queue.finishedConcat}")
+    private String finishedConcatJob;
+
+
     @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, Jackson2JsonMessageConverter converter) {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(converter);
-        return rabbitTemplate;
+    public Queue emailQueue() {
+        return new Queue(emailQueueName, true);
     }
 
     @Bean
-
-    public org.springframework.amqp.core.Queue queue(){
-        return new Queue(QUEUE_NAME);
+    public Queue videoConcatQueue() {
+        return new Queue(videoConcat, true);
     }
 
     @Bean
-    public ConnectionFactory rabbitConnectionFactory() {
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
-        connectionFactory.setHost("localhost");
-        connectionFactory.setPort(5672);
-        connectionFactory.setUsername("guest");
-        connectionFactory.setPassword("guest");
-        return connectionFactory;
+    public Queue finishedConcatQueue() {
+        return new Queue(finishedConcatJob, true);
+
     }
-
-
-    public Queue videoConcatQueue(){return new Queue(videoConcat, true);}
-
 }
