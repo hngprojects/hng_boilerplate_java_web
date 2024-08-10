7024037f1f5afac3a3b55b72c6423dbcc22e6e13
Favour Daniel <favourd205@gmail.com>
2024-08-10 13:19:28 +0100

    updated staging files

--- scripts/staging-deploy.sh
+++ scripts/staging-deploy.sh
@@ -5,4 +5,4 @@ set -e
 cd ~/hng_boilerplate_java_web/
 cp ~/docker-application.properties ~/hng_boilerplate_java_web/src/main/resources/application.properties
 cp ~/.env ~/hng_boilerplate_java_web/
-docker-compose -f staging-docker-compose.yml up --build
+docker-compose -f docker-compose.staging.yml up --build
