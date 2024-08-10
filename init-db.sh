dc81aac6f0dfdd3437c7dbf027dd815489ca1e11
Favour Daniel <favourd205@gmail.com>
2024-08-10 12:38:22 +0100

    updates

--- init-db.sh
+++ init-db.sh
@@ -3,9 +3,10 @@
 set -e
 
 # Create the admindb database
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE admindb;
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE $POSTGRES_DEV_DB;
 EOSQL
+
 
 
 # set -e
