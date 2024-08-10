96c6e99537fa41c0118ad1fbd6c9352facce0847
Favour Daniel <favourd205@gmail.com>
2024-08-10 11:18:57 +0100

    updates

--- scripts/dev-deploy.sh
+++ scripts/dev-deploy.sh
@@ -5,4 +5,5 @@ set -e
 cd ~/hng_boilerplate_java_web/
 cp ~/docker-application.properties ~/hng_boilerplate_java_web/src/main/resources/application.properties
 cp ~/.env ~/hng_boilerplate_java_web/
+docker compose down -v
 docker compose -f docker-compose.yml up --build -d
