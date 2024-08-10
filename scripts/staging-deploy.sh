ee7780eb81400f1a9e6d0bc787dae64dc4ffc6e6
Amodu David <104693505+Am0du@users.noreply.github.com>
2024-08-10 23:13:55 +0100

    Update staging-deploy.sh

--- scripts/staging-deploy.sh
+++ scripts/staging-deploy.sh
@@ -5,6 +5,6 @@ set -e
 cd ~/hng_boilerplate_java_web/
 cp ~/docker-application.properties ~/hng_boilerplate_java_web/src/main/resources/application.properties
 cp ~/.env ~/hng_boilerplate_java_web/
-./cleanup-docker.sh docker-compose.staging.yml
+# ./cleanup-docker.sh docker-compose.staging.yml
 docker-compose -f docker-compose.staging.yml down -v
 docker-compose -f docker-compose.staging.yml up --build -d
