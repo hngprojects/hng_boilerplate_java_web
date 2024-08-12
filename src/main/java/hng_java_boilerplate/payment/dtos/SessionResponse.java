c82591b32c9adde62d8e1552addbd064f4f14415
adewalekujore <adewalekujore6@gmail.com>
2024-08-12 18:59:41 +0100

    feat: updated payment checkout dto

--- src/main/java/hng_java_boilerplate/payment/dtos/SessionResponse.java
+++ src/main/java/hng_java_boilerplate/payment/dtos/SessionResponse.java
@@ -1,6 +1,10 @@
 package hng_java_boilerplate.payment.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record SessionResponse (
-        String url
+
+        @JsonProperty("checkout_url")
+        String checkoutUrl
 ){
 }
