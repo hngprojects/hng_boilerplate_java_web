863be8afec8db6b2c5f3e991a50b27b475509a46
adewalekujore <adewalekujore6@gmail.com>
2024-08-13 22:16:10 +0100

    feat: cors error

--- src/main/java/hng_java_boilerplate/plans/controller/PlanController.java
+++ src/main/java/hng_java_boilerplate/plans/controller/PlanController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("api/v1/payment/plans")
 @RequiredArgsConstructor
 @Tag(name = "Plans")
