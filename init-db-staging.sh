95fee4f31efb846c3c2b075ea92684ca8ee5fae8
stephennwachukwu <stephennwac007@gmail.com>
2024-08-15 10:38:42 +0100

    latest updates from dev

--- init-db-staging.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# Create the admindb database
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE $POSTGRES_STAGING_DB;
-EOSQL
