ec510907acbe0164b2734619852206ecde56bdac
Fifetoyi <fifetoyi@gmail.com>
2024-08-24 13:58:52 +0100

    fix: clean up

--- src/main/java/hng_java_boilerplate/Program.java
+++ src/main/java/hng_java_boilerplate/Program.java
@@ -2,10 +2,8 @@ package hng_java_boilerplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class Program {
 
 	public static void main(String[] args) {
