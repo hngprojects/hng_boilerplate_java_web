30308f1173ac838ac18a88af47035e1341399c1a
ofektom <okpohot@gmail.com>
2024-08-24 22:58:38 +0100

    fix: migration table fix

--- src/main/java/hng_java_boilerplate/user/serviceImpl/UserServiceImpl.java
+++ src/main/java/hng_java_boilerplate/user/serviceImpl/UserServiceImpl.java
@@ -162,18 +162,6 @@ public class UserServiceImpl implements UserDetailsService, UserService {
         verificationTokenRepository.save(verificationToken);
     }
 
-    @Override
-    public void requestToken(EmailSenderDto emailSenderDto, HttpServletRequest request) {
-        User user = findUserByEmail(emailSenderDto.getEmail());
-        String token = emailService.generateToken();
-        saveVerificationTokenForUser(user, token);
-        emailService.sendVerificationEmail(user, request, token);
-    }
-
-    public void saveVerificationTokenForUser(User user, String token) {
-        VerificationToken verificationToken = new VerificationToken(user, token);
-        verificationTokenRepository.save(verificationToken);
-    }
 
     @Override
     public ResponseEntity<String> verifyOtp(String email, String token, HttpServletRequest request) {
